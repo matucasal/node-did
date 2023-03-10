24b2e5e0912814a7fbc75869fd628426ca6e4642
matucasal <matias.casal@yahoo.com.ar>
2023-03-10 11:45:56 +0100

    create did and resolve it

--- app.js
+++ app.js
@@ -1,18 +1,20 @@
-import  http from 'http';
+import http from "http";
 //const { did } = require('./did.js')
 
-import did from './did.js'
+import { did, createIdentity, resolveDid } from "./did.js";
 
-const hostname = '127.0.0.1';
+const hostname = "127.0.0.1";
 const port = 3000;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hola Mundo');
+  res.setHeader("Content-Type", "text/plain");
+  res.end("Hola Mundo");
 });
 
 server.listen(port, hostname, () => {
   console.log(`El servidor se está ejecutando en http://${hostname}:${port}/`);
-  did();
+  const identity = createIdentity();
+  resolveDid(identity);
+  //did();
 });
--- did.js
+++ did.js
@@ -5,7 +5,7 @@ import { CryptoLD } from "crypto-ld";
 import { Resolver } from "did-resolver";
 import ethr from "ethr-did-resolver";
 
-
+import { Credentials } from "uport-credentials";
 
 const config = {
   networks: [
@@ -24,19 +24,74 @@ const did = async () => {
     type: "Ed25519VerificationKey2020",
   });
   console.log("keyPair generated ", keyPair);
-  const keyPairExported = keyPair.export({publicKey: true});
-  console.log('keyPairExported', keyPairExported);
+  const keyPairExported = keyPair.export({ publicKey: true });
+  console.log("keyPairExported", keyPairExported);
+  const { verify } = await keyPair.verifier();
+  console.log("verify", verify);
+
   const did = `did:ethr:${keyPairExported.publicKeyMultibase}`;
-  console.log('did generated ', did);
+  console.log("did generated ", did);
   // create a DID using the matic/mumbai blockchain as the resolver
   const ethrResolver = ethr.getResolver(config);
   const resolver = new Resolver({
     ...ethrResolver,
   });
 
-  console.log('resolver created', resolver);
+  console.log("resolver created", resolver);
 
-  
+  console.log("start the resolving process");
+  resolver.resolve(did).then((doc) => {
+    console.log("doc", doc);
+  });
 };
 
-export default did;
+
+export function addPrefix(prefixToAdd, did) {
+  const prefixedDid = did.slice(0, 9) + prefixToAdd + did.slice(9, did.length);
+  return prefixedDid;
+}
+
+const resolveDid = (did) => {
+  const ethrResolver = ethr.getResolver(config);
+  const resolver = new Resolver({
+    ...ethrResolver,
+  });
+
+  console.log("resolver created", resolver);
+
+  console.log('did to resolve', did);
+
+  console.log("start the resolving process");
+  resolver.resolve(did.did).then((doc) => {
+    console.log("doc", doc);
+  });
+};
+
+const createIdentity = (prefixToAdd = "mumbai") => {
+  let prefixChecked = false;
+  let prefixedDid = null;
+
+  /*
+  if (prefixToAdd) {
+    prefixChecked = checkPrefix(
+      prefixToAdd,
+      this.config.providerConfig.networks
+    );
+    if (!prefixChecked) {
+      throw new Error(
+        "Invalid Prefix - Check Provider Network Configuration"
+      );
+    }
+  }*/
+  const credential = Credentials.createIdentity();
+
+  if (prefixToAdd) {
+    prefixedDid = addPrefix(`${prefixToAdd}:`, credential.did);
+    credential.did = prefixedDid;
+  }
+  console.log("credential", credential);
+  return credential;
+};
+
+//export default did;
+export { did, createIdentity, resolveDid };
